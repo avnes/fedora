78f38fc5266a3808c260784896c028ae8377ea49
Audun Nes <audun.nes@gmail.com>
2017-04-14 17:13:43 +0200

    Add box for https://github.com/avnes/dixie

--- custom-script.sh
+++ custom-script.sh
@@ -7,3 +7,22 @@ set -eux
 #
 # For example:
 # dnf install -y curl wget git tmux firefox xvfb
+
+dnf update -y
+
+dnf install -y @base-x
+dnf install -y @lxde-desktop
+dnf install -y @Fonts
+dnf install -y python2 python2-dnf libselinux-python bzip2 chromium
+dnf install -y convert-to-edition curl git gzip keepassx lsof nmap subversion
+dnf install -y sudo sysstat tar unzip vim wget which zip lxdm
+
+convert-to-edition -p -e workstation
+
+dnf update -y
+
+#systemctl set-default graphical.target
+
+systemctl enable sshd
+
+systemctl start sshd
